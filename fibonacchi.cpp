2f546f0daf8714d68cd27f82e10db0d35a18f914
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2022-11-16 21:28:29 +0530

    Fibonacci Series

---
+++ fibonacchi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+
+    return fibo(n-1) + fibo(n-2);
+}
+
+
+int main(){
+    int n = 3;
+    cout<<fibo(n)<<endl;
+
+    return 0;
+}
