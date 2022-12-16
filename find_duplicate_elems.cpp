12cd212d78e16991ca4413f51f955e90e635be2c
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2022-12-16 13:14:28 +0530

    Bruteforce Approach

---
+++ find_duplicate_elems.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {3,1,3,4,2};
+
+    //Bruteforce approach
+
+    // for (int i = 0; i < nums.size(); i++)
+    // {
+    //     for (int j = 0; j < nums.size(); j++)
+    //     {
+    //         if(i==j)
+    //             continue;
+    //         if(nums[i]==nums[j]){
+    //             cout<<nums[i];
+    //             break;
+    //         }
+    //     }
+        
+    // }
+    
+    
+    
+
+
+    return 0;
+}
