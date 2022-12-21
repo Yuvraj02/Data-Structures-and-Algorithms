e5821a6a54971c9fb8cf39b2a1534971f68b06d2
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2022-12-21 21:45:01 +0530

    Codechef Contest

---
+++ kitchen_timings.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int X,Y;
+        cin>>X>>Y;
+        cout<<Y-X;
+    }
+        
+
+    return 0;
+}
