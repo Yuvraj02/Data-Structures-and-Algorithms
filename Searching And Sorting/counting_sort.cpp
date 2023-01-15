8bf51bcf306ba9ab1f714d6730843ad0d16491ab
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2023-01-15 19:29:29 +0530

    Stable Counting sort

--- Searching And Sorting/counting_sort.cpp
+++ Searching And Sorting/counting_sort.cpp
@@ -6,39 +6,31 @@ using namespace std;
 
 int main(){
 
-    vector<int> A = {1,4,1,2,7,5,2};
-    int minVal = *min_element(A.begin(),A.end());
+    vector<int> A = {-1 ,2 ,3 ,-4 ,-5};
+    
     int maxVal = *max_element(A.begin(),A.end());
+    int minVal = *min_element(A.begin(),A.end());
+    int range = maxVal-minVal+1;
 
-    int range;
-    if(minVal<0)
-    range = maxVal-minVal+1;
-    else
-    range = maxVal+2;
+    vector<int> count(range,0), output(A.size(),0);
 
-    vector<int> count(range), output;
-
+    //store count of each value
     for (int i = 0; i < A.size(); i++)
     {
-        if(minVal<0)
-            count[A[i]-minVal]++;
-        else
-            count[A[i]]++;
+        count[A[i]-minVal]++;
     }
-
-    int i=0;
-    while (i<count.size())
+    
+    //Add up all values
+    for (int i = 1; i < count.size(); i++)
     {
-        if(count[i]!=0){
-            if(minVal<0){
-                output.push_back(minVal+i);
-            }else{
-                output.push_back(i);
-            }
-            count[i]--;
-        }else{
-            i++;
-        }
+        count[i]+=count[i-1];
+    }
+    
+    //store in output array;
+    for (int i = A.size()-1; i >= 0; i--)
+    {
+        output[count[A[i]-minVal]-1] = A[i];
+        count[A[i]-minVal]--;
     }
     
     for(auto i : output){
