7fb94ae42adf6e85577811e253de8c4cd2e77673
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2023-01-15 19:32:17 +0530

    Check if reversing subarray makes array sorted

---
+++ Searching And Sorting/check_if_reversing_subarray_make_array_equal.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverse(vector<int>& A, int left, int right){
+
+    int i = left,j=right;
+    cout<<left<<" "<<right<<endl;
+    while(i<=j){
+        swap(A[i],A[j]);
+        i++;
+        j--;
+    }
+}
+
+int main(){
+
+    vector<int> A = {1,2,4,5,3};
+
+    int left=-1,right=-1;
+
+    for (int i = 0; i < A.size(); i++)
+    {
+        if(A[i]>A[i+1]){
+            if(left==-1){
+                left=i;
+            }
+            right=i;
+        }
+    }
+    
+    if(left!=-1){
+        reverse(A,left,right);
+    }
+    
+    bool isPossible = true;    
+    for(int i = 0; i< A.size()-1;i++){
+        if(A[i]>A[i+1]){
+            isPossible = false;
+            break;
+        }
+    }
+
+    if(isPossible)
+        cout<<"YES";
+    else
+    cout<<"NO";
+
+    return 0;
+}
