b772c245acffb6e31c7043abec8ef60ff2cb4149
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2023-01-20 23:48:17 +0530

    Climb Stairs problem using recursion

---
+++ recursion/climb_stairs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int climbStairs(int n){
+
+    if(n<0) return 0;
+    if(n==0) return 1;
+
+    return climbStairs(n-1) + climbStairs(n-2);
+}
+
+int main(){
+
+    cout<<climbStairs(3);
+    return 0;
+}
