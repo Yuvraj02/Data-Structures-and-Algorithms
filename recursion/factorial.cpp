370555fcf223e2d7d79088c266bc02271e431b19
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2023-01-20 23:46:39 +0530

    Factorial

---
+++ recursion/factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n){
+    if(n==0)
+        return 1;
+
+    return n*factorial(n-1);
+}
+
+
+int main(){
+
+    cout<<factorial(10);
+
+    return 0;
+}
