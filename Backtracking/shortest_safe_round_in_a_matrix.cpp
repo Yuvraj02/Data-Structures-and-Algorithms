73e8ef9cd72e01902f2734b3f5bed5628494bc57
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2023-01-29 17:06:15 +0530

    Backtracking

--- Backtracking/shortest_safe_round_in_a_matrix.cpp
+++ Backtracking/shortest_safe_round_in_a_matrix.cpp
@@ -2,87 +2,102 @@
 #include <vector>
 using namespace std;
 
+bool isSafe(vector<vector<int>> A, int x, int y, vector<vector<int>>& helperMaze){
 
-bool isSafeMove(vector<vector<int>>& A,int x, int y, vector<vector<int>> &helperMaze){
-
-    if((x>=0 && x<A.size()) && (y>=0 && y<A[0].size())){
-
-        if(A[x][y]==0)
-            return false;
-        //Check For directions
-        
-        //Check for right side
-        if(y+1<A[0].size() && A[x][y+1]!=1)
-            return false;
-        //Check For Down side
-        if(x+1<A.size() && A[x+1][y]!=1)
-            return false;
-        //Check for Left side
-        if(y-1>=0 && A[x][y-1]!=1)
-            return false;
-        //Check for upside
-        if(x-1>=0 && A[x-1][y]!=1)
-            return false;
-
-        if(helperMaze[x][y]!=0)
-            return false;
-    }else{
+    if(A[x][y]==0 || helperMaze[x][y]==1)
         return false;
-    }
 
-    return true;
+return true;
 }
 
-void solve(vector<vector<int>> A, int x, int y, vector<vector<int>> &helperMaze,vector<int> moveX,vector<int> moveY,int &shortestPath,int &count){
+bool isValid(int n,int m, int x, int y){
 
+    if((x>=0 && x<n) && (y>=0 && y<m))
+        return true;
+    
+    return false;
+}
+
+void markUnsafeCells(vector<vector<int>>& A, vector<int> xMove, vector<int> yMove){
+
+//Mark the Adjacent cells with landmine
+for (int i = 0; i < A.size(); i++)
+{
+    for (int j = 0; j < A[0].size(); j++)
+    {
+        if(A[i][j]==0){
+            for(int k = 0; k<4;k++){
+                    if(isValid(A.size(),A[0].size(),i+xMove[k],j+yMove[k]))
+                        A[i+xMove[k]][j+yMove[k]]=-1;
+            }
+        }
+    }
+}
+
+for(int i=0;i<A.size();i++){
+        for(int j=0;j<A[0].size();j++){
+            if(A[i][j]==-1)
+                A[i][j]=0;
+        }
+    }
+
+}
+
+void solve(vector<vector<int>> A, int x, int y, vector<vector<int>> &helperMaze,vector<int> moveX,vector<int> moveY,int &shortestPath,int count){
+   
     if(y==A[0].size()-1){
         //reached state
+       // cout<<"Found";
         shortestPath = min(count,shortestPath);
         return;
     }
 
-    for (int row = x; row < A.size(); row++)
+    helperMaze[x][y] = 1;
+    
+    for (int i = 0; i < 4; i++)
     {
-        //Check for all directions
-        for (int k = 0; k < 4; k++)
-        {
-            int next_X = row + moveX[k];
-            int next_Y = y + moveY[k];
-            if(isSafeMove(A,next_X,next_Y,helperMaze)){
-                count++;
-                helperMaze[next_X][next_Y]=1;     
-                solve(A,next_X,next_Y,helperMaze,moveX,moveY,shortestPath,count);
-                helperMaze[next_X][next_Y] = 0;
-                count--;
-            }
-
+        int next_x = x+moveX[i];
+        int next_y = y+moveY[i];
+        if(isValid(A.size(),A[0].size(),next_x,next_y) && isSafe(A,next_x,next_y,helperMaze)){
+            //cout<<"Nope";
+            solve(A,next_x,next_y,helperMaze,moveX,moveY,shortestPath,count+1);
         }
     }
+    
+    helperMaze[x][y]=0;
 
 }
 
 int main(){
 
-    vector<vector<int>> A = {{1,0,1,1,1},
-                             {1,1,1,1,1},
-                             {1,1,1,1,1},
-                             {1,1,1,0,1},
-                             {1,1,1,1,0}};
-    vector<vector<int>> helperMaze (A.size(), vector<int>(A[0].size(),0));
+    vector<vector<int>> A = {{ 1  ,1 , 1 , 1 , 1 , 1 , 1 , 1 , 1,  1,},
+                             { 1 , 0 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1,},
+                             { 1 , 1 , 1 , 0 , 1 , 1 , 1 , 1 , 1 , 1 },
+                             { 1 , 1 , 1 , 1 , 0 , 1 , 1 , 1 , 1 , 1 },
+                             { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 },
+                             { 1 , 1 , 1 , 1 , 1 , 0 , 1 , 1 , 1 , 1 },
+                             { 1 , 0 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1 },
+                             { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 },
+                             { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 },
+                             { 0 , 1 , 1 , 1 , 1 , 0 , 1 , 1 , 1 , 1 },
+                             { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 },
+                             { 1 , 1 , 1 , 0 , 1 , 1 , 1 , 1 , 1 , 1 }};
 
+    vector<vector<int>> helperMaze (A.size(), vector<int>(A[0].size(),0));
     vector<int> moveX = {0, 0, 1,-1};
     vector<int> moveY = {1,-1, 0, 0};
         
-    for(int i=0;i<A.size();i++){
-        for(int j=0;j<A[0].size();j++){
-            cout<<helperMaze[i][j]<<" ";
-        }
-        cout<<endl;
-    }  
-
     int shortestPath = INT32_MAX;
-    int count = 1;
-    solve(A,0,0,helperMaze,moveX,moveY,shortestPath,count);
+    int count = 0;
+    
+    markUnsafeCells(A,moveX,moveY);
+
+    for (int i = 0; i < A.size(); i++)
+    {
+         if(A[i][0]==1)
+            solve(A,i,0,helperMaze,moveX,moveY,shortestPath,count);
+    }
+
     cout<<shortestPath;
     return 0;
 }
