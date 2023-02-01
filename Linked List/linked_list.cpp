71b4f86178f605fa9b4a004a74a6d0aad2812043
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2023-02-02 01:06:20 +0530

    Linked List

--- Linked List/linked_list.cpp
+++ Linked List/linked_list.cpp
@@ -8,7 +8,7 @@ class Node{
     public:
         int val;
         Node *next_node;
-
+        
     //Constructor
     Node(int val){
         this->next_node = nullptr;
@@ -17,11 +17,106 @@ class Node{
 
 };
 
+void insertAtHead(Node* &head,int val){
+        Node *newNode = new Node(val);
+        newNode->next_node = head;
+        head = newNode;
+}
+
+void insertAtTail(Node* &tail, int val){
+    Node *newNode = new Node(val);
+    tail->next_node = newNode;
+    tail = newNode;
+}
+
+void insertAt(Node* &head,Node* &tail,int i, int val){
+
+    if(i==1){
+        insertAtHead(head,val);
+        return;
+    }
+
+    Node *newNode = new Node(val);
+    Node *temp = head;
+    int index = 1;
+    while (index<i-1)
+    {
+        temp = temp->next_node;
+        index++;
+    }
+
+    if(temp->next_node==nullptr){
+        insertAtTail(tail,val);
+    }else{
+        newNode->next_node = temp->next_node;
+        temp->next_node = newNode;
+    }
+    
+}
+
+void deleteAt(Node* &head, Node* &tail,int position){
+        Node *temp = head;
+
+        if(position==1){
+            head = head->next_node;
+            delete temp;
+            return ;
+        }
+
+        int index = 1;
+        while (index<position-1)
+        {
+            temp = temp->next_node;
+            index++;
+        }
+
+        if(temp->next_node->next_node == nullptr){
+            
+            tail = temp;
+            temp = temp->next_node;
+            tail->next_node=nullptr;
+            delete temp;
+            return ;
+        }
+
+        Node *nodeToDelete = temp->next_node;
+        temp->next_node = nodeToDelete->next_node;
+
+        delete nodeToDelete;
+}
+
+void printList(Node* &head){
+
+    Node *temp = head;
+
+    while (temp != nullptr)
+    {
+        cout<<temp->val<<"->";
+        temp = temp->next_node;
+    }
+
+    cout<<"NULL";
+    
+}
+
+
 
 int main(){
 
-    Node *node1 = new Node(10);
-    cout<<node1->val;
+    Node *head = new Node(10);
+    Node *tail = head;
+    insertAtTail(tail,20);
+    insertAtTail(tail,30);
+    insertAtHead(head,5);
+    insertAt(head,tail,2,7);
+    
+    printList(head);
+
+    deleteAt(head,tail,5);
+    cout<<endl;
+    printList(head);
+
+//    cout<<head->val;
 
     return 0;
 }
