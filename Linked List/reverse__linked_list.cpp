9b0f2667b4100ec4c1d45e02b5f1dcabe72503c5
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2023-02-06 19:13:43 +0530

    Iterative Approach

---
+++ Linked List/reverse__linked_list.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+class LinkedList{
+
+    public:
+        int data;
+        LinkedList* nextNode;
+
+    LinkedList(int data){
+        this->data = data;
+        this->nextNode = NULL;
+    }
+};
+
+void insertAtTail(LinkedList* &tail, int val){
+
+    LinkedList *newNode = new LinkedList(val);
+    tail->nextNode = newNode;
+    tail = newNode;
+
+}
+
+void printList(LinkedList* &head){
+
+    LinkedList *temp = head;
+
+    while (temp!=NULL)
+    {
+        cout<<temp->data<<"--->";
+        temp = temp->nextNode;
+    }
+    cout<<"NULL"<<endl;
+}
+
+
+//Iterative Approach
+// void reverseLinkedList(LinkedList* &head){
+
+//     LinkedList *prev = NULL;
+//     LinkedList *current = head;
+//     LinkedList *next = current->nextNode;
+
+//     while (next!=NULL)
+//     {
+//         current->nextNode = prev;
+//         prev = current;
+//         current = next;
+//         next = next->nextNode;
+
+//     }
+    
+//     current->nextNode = prev;
+//     head = current;
+// }
+
+int main(){
+
+    LinkedList *head = new LinkedList(1);
+    LinkedList *tail = head;
+
+    for(int i = 2; i<=6;i++)
+        insertAtTail(tail,i);
+
+    //reverseLinkedList(head);
+
+    printList(head);
+    return 0;
+}
