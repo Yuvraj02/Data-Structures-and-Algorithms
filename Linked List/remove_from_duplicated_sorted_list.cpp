b7d0090db9d87f537142bec8437363b1510f4270
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2023-02-09 21:59:32 +0530

    Duplicate Sorted List

--- Linked List/remove_from_duplicated_sorted_list.cpp
+++ Linked List/remove_from_duplicated_sorted_list.cpp
@@ -34,7 +34,21 @@ void printList(Node *head){
 
 }
 
+void removeDuplicates(Node* &head){
+   
+    Node *temp = head;
 
+    while (temp!=NULL)
+    {
+       if(temp->next!=NULL && temp->data==temp->next->data){
+            Node *node_to_delete = temp->next;
+            temp->next = temp->next->next;
+            delete node_to_delete;
+       }else{
+        temp = temp->next;
+       }
+    }
+}
 
 int main(){
 
@@ -42,10 +56,17 @@ int main(){
     Node *tail = head;
 
     insertAtTail(tail,2);
+    insertAtTail(tail,2);
+    insertAtTail(tail,2);
     insertAtTail(tail,3);
     insertAtTail(tail,3);
     insertAtTail(tail,3);
     insertAtTail(tail,4);
+    insertAtTail(tail,4);
+    insertAtTail(tail,5);
+    insertAtTail(tail,5);
+    insertAtTail(tail,6);
+    removeDuplicates(head);
     printList(head);
 
     return 0;
