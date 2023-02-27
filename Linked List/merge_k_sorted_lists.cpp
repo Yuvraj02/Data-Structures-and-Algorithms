683409e94d10ff156a7ef9951e34c0ceb06a31b9
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-02-27 23:59:22 +0530

    Merge K sorted List using Divide and Conquor

--- Linked List/merge_k_sorted_lists.cpp
+++ Linked List/merge_k_sorted_lists.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 class Node{
 
     public:
@@ -14,6 +13,10 @@ class Node{
     }
 
 };
+
+
+Node *mergeKLists(vector<Node*>&, int);
+Node *merge(Node* , Node*);
 
 void insertAtTail(Node* &tail, int val){
     Node *newNode = new Node(val);
@@ -30,6 +33,43 @@ void printList(Node *head){
     cout<<"NULL"<<endl;
 }
 
+//Time : O(N log K), Space : O(1)
+
+Node* mergeKListsOptimal(vector<Node*>& lists, int start, int end){
+    if(start>=end)   
+        return lists[start];
+    
+    int mid = start + (end-start)/2;
+    return merge(mergeKListsOptimal(lists,start,mid),mergeKListsOptimal(lists,mid+1,end));
+}
+
+int main(){
+
+    Node *l1 = new Node(1);
+    Node *tail1 = l1;
+    insertAtTail(tail1,4);
+    insertAtTail(tail1,5);
+    Node *l2 = new Node(1);
+    Node *tail2 = l2;
+    insertAtTail(tail2,3);
+    insertAtTail(tail2,4);
+    Node *l3 = new Node(2);
+    Node *tail3 = l3;
+    insertAtTail(tail3,6);
+    
+    vector<Node*> lists = {l1,l2,l3};
+
+    for (int i = 0; i < lists.size(); i++)
+    {
+        printList(lists[i]);
+    }
+    Node *sortedLists = mergeKListsOptimal(lists, 0,lists.size()-1);
+    printList(sortedLists);
+    return 0;
+}
+
+
+//Bruteforce Recursive Solution
 Node *merge(Node *first, Node* second){
 
     Node *temp = new Node(-1);
@@ -59,32 +99,4 @@ Node *mergeKLists(vector<Node*>&lists, int i){
 
     return merge(lists[i], mergeKLists(lists,i+1));
 
-}
-
-int main(){
-
-    Node *l1 = new Node(1);
-    Node *tail1 = l1;
-    insertAtTail(tail1,4);
-    insertAtTail(tail1,5);
-    Node *l2 = new Node(1);
-    Node *tail2 = l2;
-    insertAtTail(tail2,3);
-    insertAtTail(tail2,4);
-    Node *l3 = new Node(2);
-    Node *tail3 = l3;
-    insertAtTail(tail3,6);
-    
-    vector<Node*> lists = {l1,l2,l3};
-
-    for (int i = 0; i < lists.size(); i++)
-    {
-        /* code */
-        printList(lists[i]);
-    }
-    Node *sortedLists = mergeKLists(lists, 0);
-    printList(sortedLists);
-    
-
-    return 0;
 }
