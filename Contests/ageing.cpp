cccec8d5d9b6d8e7bcc697c1dc163d71cdbc23d5
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-03-01 22:27:06 +0530

    Ageing

---
+++ Contests/ageing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int X;
+	    cin>>X;
+	    cout<<10 + abs(20-X)<<endl;
+	}
+	
+	return 0;
+}
