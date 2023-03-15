cf0cbd6db0484958b8d094469386c6784735c28e
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-03-15 18:19:28 +0530

    Whether one stack is permuation of other or not

---
+++ Stack/stack_permutation.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int isStackPermutation(int N,vector<int> &A,vector<int> &B){
+        int i = 0,j=0;
+        stack<int> s;
+        
+        for(int i = 0;i < N; i++){
+            s.push(A[i]);
+            while(!s.empty() && B[j]==s.top()){
+                s.pop();
+                j++;
+            }
+        }
+        
+        if(s.empty() && j>=B.size())
+            return true;
+        else
+            return false;
+        
+    }
+
+int main(){
+
+    int N = 3;
+    vector<int> A = {1,2,3};
+    vector<int> B = {1,2,3};
+
+    cout<<isStackPermutation(N,A,B);
+    return 0;
+}
