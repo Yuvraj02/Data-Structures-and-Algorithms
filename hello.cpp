1c6105c08e8ab9b8db8a7c92d2faac259692fa82
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-03-24 14:35:04 +0530

    Power function Log N solution

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+
 using namespace std;
+
+//Log N solution
+int pow(int a, int b){
+
+    if(b==0)
+        return 1;
+    if(b==1)
+        return a;
+
+    if(b%2==0)
+        return pow(a,b/2) * pow(a,b/2);
+    else
+        return a * pow(a,b/2) * pow(a,b/2);
+}
 
 int main(){
 
-    cout<<"Hello World";
+    cout<<pow(2,5)<<endl;
+
     return 0;
 }
