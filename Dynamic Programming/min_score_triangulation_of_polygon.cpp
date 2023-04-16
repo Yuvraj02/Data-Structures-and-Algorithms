9aad2d7ba9065f9913d1778f5b9636176b54fe88
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-04-16 18:12:57 +0530

    Min Score Triangulation Memoization

--- Dynamic Programming/min_score_triangulation_of_polygon.cpp
+++ Dynamic Programming/min_score_triangulation_of_polygon.cpp
@@ -4,28 +4,31 @@
 
 using namespace std;
 
-int minScoreTriangulation(vector<int> &values,int i, int j,vector<vector<int>> &dp){
+int solve(vector<int> values, int i,int j, vector<vector<int>> &dp){
 
-    if(i+1==j)
+if(i+1==j)
         return 0;
 
     if(dp[i][j]!=INT32_MAX)
         return dp[i][j];
 
     for(int k = i+1; k<j;k++){
-        dp[i][j] = min({dp[i][j],values[i]*values[j]*values[k] + minScoreTriangulation(values,i,k,dp)+minScoreTriangulation(values,k,j,dp)});
+        dp[i][j] = min({dp[i][j],values[i]*values[j]*values[k] + solve(values,i,k,dp)+solve(values,k,j,dp)});
     }
     return dp[i][j];
+}
+
+int minScoreTriangulation(vector<int> &values){
+    int n = values.size();
+    vector<vector<int>> dp(n, vector<int>(n,0));
+    // return solve(values,0,n-1,dp); 
+
 }
 
 int main(){
 
     vector<int> values = {3,7,4,5};
-    int n = values.size();
-    vector<vector<int>> dp(n+1, vector<int>(n+1,INT32_MAX));
-    
-
-    cout<<minScoreTriangulation(values,0,n-1,dp);
+    cout<<minScoreTriangulation(values);   
 
     return 0;
 }
