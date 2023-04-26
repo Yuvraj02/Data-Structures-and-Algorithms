05cd8c1ca17bdc37db118b4d3dfb70d0a9842161
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-04-26 17:54:40 +0530

    Unique Binary Search Tree

---
+++ Dynamic Programming/unique_binary_search_trees.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+ int solve(int n){
+        if(n<=1)
+            return 1;
+
+        int ans= 0;
+        for(int i = 1; i <= n; i++){
+            ans += solve(i-1) * solve(n-i);
+        }
+
+        return ans;
+}
+
+int solveMem(int n,vector<int>& dp){
+
+    if(n<=1)
+        return 1;
+
+    if(dp[n]!=-1)
+        return dp[n];
+
+    int ans = 0;
+    for(int i = 1; i<=n; i++)
+        ans += solveMem(i-1,dp) * solveMem(n-i,dp);
+
+    return dp[n] = ans;
+
+}
+
+ int solveTab(int n) {
+        vector<int> dp(n+1, 1);
+
+        for(int i = 2; i<=n; i++){
+            int ans = 0;
+            for(int j=1; j<=i;j++){
+                ans+=dp[j-1] * dp[i-j];
+            }
+            dp[i] = ans;
+        }
+
+        //return solve(n,dp);
+        return dp[n];
+    }
+
+int main(){ 
+
+    int n = 3;
+
+    //vector<int> dp(n+1,-1);
+    //cout<<solveMem(n,dp);
+    cout<<solveTab(n);
+    return 0;
+}
+
