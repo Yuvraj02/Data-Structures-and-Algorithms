0f509d58a0bb7afa532c5bf50b2a9b4e00bf93f7
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-05-16 16:52:30 +0530

    Postorder Traversal

---
+++ Binary Tree/postorder_traversal.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        Node *left;
+        Node *right;
+        int data;
+
+        Node(int data){
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+
+};
+
+Node *buildTree(Node *root){
+
+    cout<<"Enter the data: ";
+    int data;
+    cin>>data;
+
+    if(data==-1)
+        return NULL;
+
+    root = new Node(data);
+
+    cout<<"Going to the left of "<<data<<endl;
+    root->left = buildTree(root->left);
+
+    cout<<"Going to the right of "<<data<<endl;
+    root->right = buildTree(root->right);
+
+    return root;
+}
+
+void preOrderTraversal(Node *root){
+
+    if(root==NULL)
+        return ;
+    
+
+    //Going left
+    preOrderTraversal(root->left);
+    
+    preOrderTraversal(root->right);
+    
+    cout<<root->data<<" ";
+}
+
+
+int main(){
+
+    Node *root = NULL;
+    root = buildTree(root);
+    /*
+    --> 10 20 40 -1 -1 60 -1 -1 30 -1 -1
+
+        10
+      /  \
+     20   30
+    / \ 
+   40  60
+    */
+    preOrderTraversal(root);
+    return 0;
+}
