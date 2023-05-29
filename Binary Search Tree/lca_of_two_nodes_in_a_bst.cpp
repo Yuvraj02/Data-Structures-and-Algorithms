2a99b4789f79c09e6ea9068efc2b24ef64a5cf76
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-05-29 17:53:13 +0530

    LCA Of Two nodes in a BST

---
+++ Binary Search Tree/lca_of_two_nodes_in_a_bst.cpp
@@ -0,0 +1,54 @@
+  #include <iostream>
+  
+  using namespace std;
+
+  
+  template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* p, TreeNode<int>* q)
+{
+	//Write your code here
+    if(root==NULL)
+        return NULL;
+
+        if(root->data > p->data && root->data > q->data){
+                return LCAinaBST(root->left, p, q);
+        } 
+        if(root->data < p->data && root->data < q->data){
+                return LCAinaBST(root->right, p, q);
+        } 
+        
+        return root;
+
+    // while(root){
+
+    //     if(root->data > p->data && root->data > q->data){
+    //             root = root->left;
+    //     } else if (root->data < p->data && root->data < q->data) {
+    //       root = root->right;
+    //     } else{
+    //       return root;
+    // }
+    // }
+
+}
