28f2d79fdb7a019336cb1b2af5efc155238a3055
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-07-01 21:07:00 +0530

    Minimum Cost of ropes

---
+++ Heaps/minimum_cost_of_ropes.cpp
@@ -0,0 +1,33 @@
+ #include <iostream>
+ #include <queue>
+ 
+ using namespace std;
+ 
+ long long minCost(long long arr[], long long n) {
+        // Your code here
+        
+        priority_queue<long long,vector<long long>,greater<long long>> pq;
+        
+        for(long long i = 0;i<n;i++){
+            pq.push(arr[i]);
+        }
+        
+        long long cost = 0;
+        
+        while(pq.size()>1){
+            //Take two top elements, add them and push back
+            long long a = pq.top();
+            pq.pop();
+            
+            long long b = pq.top();
+            pq.pop();
+            
+            cost+=a+b;
+            
+            pq.push(a+b);
+        }
+        
+        
+        return cost;
+        
+    }
