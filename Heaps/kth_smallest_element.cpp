463022b7e3933acc9dc427998b1750cd8a873c29
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-07-01 21:06:12 +0530

    Kth Smallest Element using Heap

---
+++ Heaps/kth_smallest_element.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main(){
+
+    vector<int> A = {7,10,4,3,20,15};
+    int k = 3;
+
+    priority_queue<int> pq;
+
+    for(int i = 0;i<k;i++)
+        pq.push(A[i]);
+    
+    for(int i = k; i<A.size();i++){
+        if(A[i] < pq.top()){
+            pq.pop();
+            pq.push(A[i]);
+        }
+    }
+
+    cout<<pq.top();
+    
+    return 0;
+}
