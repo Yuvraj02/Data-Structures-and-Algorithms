900d7dc1efc26d57417b4d651f3baa0ed5a726b2
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-07-06 17:18:40 +0530

    Construct a BST from preorder

--- Binary Search Tree/construct_bst_from_preorder.cpp
+++ Binary Search Tree/construct_bst_from_preorder.cpp
@@ -45,20 +45,10 @@ BinaryTreeNode<int> *insertNode(int &i, vector<int> &preorder,int mini, int maxi
 
 
 BinaryTreeNode<int>* preorderToBST(vector<int> &preorder) {
-    // Write your code here.
 
     int mini = INT32_MIN;
     
     int maxi = INT32_MAX;
     int i = 0;
     return  insertNode(i, preorder, mini, maxi);
-
-    // BinaryTreeNode<int> *root = NULL;
-    // // BinaryTreeNode<int> *root = newNode;
-    // for(int i = 0; i<preorder.size();i++){
-
-    //         root = insertNode(root,preorder[i]);
-    // }
-
-    // return root;
 }
