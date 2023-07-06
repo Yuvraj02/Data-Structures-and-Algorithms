abe30e45eceb48b2ae858711314a1324983a0c3e
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-07-06 17:21:22 +0530

    Get size of largest BST

--- Binary Search Tree/size_of_largest_bst.cpp
+++ Binary Search Tree/size_of_largest_bst.cpp
@@ -23,43 +23,43 @@ using namespace std;
         }
     };
 
-
-
-
-class info {
-    
+class info{
     public:
         int maxi;
         int mini;
         bool isBST;
         int size;
-    
 };
 
-info solve(TreeNode<int>* root, int &maxSize){
-
-    if(root==NULL)
-      return {INT_MIN,INT_MAX,true,0};
-
+info traverseTree(TreeNode<int>* root, int &maxSize){
     
-    info left = solve(root->left, maxSize);
-    info right = solve(root->right, maxSize);
+    if(!root)
+      return {INT32_MIN, INT32_MAX, true,0};
 
+    info leftTree = traverseTree(root->left, maxSize);
+    info rightTree = traverseTree(root->right, maxSize);
+
+    //This will hold values from both subtrees
     info currNode;
-    currNode.size = left.size + right.size + 1;
+    //This is the total size of the tree upto current node
+    currNode.size = leftTree.size + rightTree.size + 1;
 
-    currNode.maxi = max(root->data, right.maxi);
-    currNode.mini = min(root->data, right.mini);
-
-    if(left.isBST && right.isBST && (root->data > left.maxi && root->data < right.mini)){
+    //Get Min and Max value for curr node, so that we can compare and check whether the value
+    //of current node lies within the correct range
+    //Minimum value should be minimum of left subtree
+    //and maximum value should be maximum of right subtree
+    //if this is not the case, then when we include curr node, it's not a BST
+    currNode.mini = min(root->data, leftTree.mini);
+    currNode.maxi = max(root->data, rightTree.maxi);
+    
+    //Check if the curr node is in the correct place
+    if(leftTree.isBST && rightTree.isBST &&(root->data > leftTree.maxi && root->data < rightTree.mini))
         currNode.isBST = true;
-    }else{
+    else
         currNode.isBST = false;
-    }
-
+    
     if(currNode.isBST)
         maxSize = max(maxSize,currNode.size);
-
     return currNode;
 }
 
@@ -67,6 +67,7 @@ int largestBST(TreeNode<int>* root)
 {
     // Write your code here.
     int maxSize = 0;
-    solve(root,maxSize);
+    traverseTree(root,maxSize);
     return maxSize;
+
 }
