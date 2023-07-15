4ca7dd9115a311490ea2723e250fb77465d668b5
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-07-15 23:57:42 +0530

    LIS DP

--- Dynamic Programming/longest_arithmetic_subsequence.cpp
+++ Dynamic Programming/longest_arithmetic_subsequence.cpp
@@ -3,45 +3,58 @@
   #include <unordered_map>
 
   using namespace std;
-  
-  int solve(int index, vector<int>& arr, int difference,vector<int>&dp){
-        if(index < 0)
+  //DP Solution
+  int largestFrom(int i, vector<int> &arr, int diff,int n,vector<int> &dp){
+        if(i>=n-1)
             return 0;
 
-        if(dp[index] != -1)
-            return dp[index];
+        if(dp[i]!=-1)
+            return dp[i];
 
         int ans = 0;
-        for(int j = index-1;j>=0;j--){
-            if(arr[index]-arr[j]==difference)
-                ans = max(ans, 1 + solve(j,arr,difference,dp));
+        for(int j = i+1; j < n;j++){
+            if(arr[j]-arr[i]==diff)
+                ans = max(ans,1 + largestFrom(j,arr,diff,n,dp));
         }
 
-        dp[index] = ans;
-        return dp[index];
+        return dp[i] = ans;
+    }
+//Tabulation 
+    int larestFromTab(vector<int>& arr, int diff){
+        int n =arr.size();
+
+        vector<int> dp(n+1,0);
+        int maxLength = 0;
+        for(int i = 0; i<arr.size();i++){
+
+            for(int j = n-2; j>=0;j--){
+                int ans = 0;
+                for(int k = j+1; k<n;k++){
+                    if(arr[k]-arr[j]==diff)
+                        ans = max(ans,1 + dp[k]);
+                }
+                dp[j] = ans;
+            }
+            maxLength = max(1+dp[i],maxLength);
+        }
+        return maxLength;
     }
 
-    int longestSubsequence(vector<int>& arr, int difference) {
-        int n = arr.size();
-        if(n<=2)
-            return n;
+//Space optimization
+    int longestSubsequence(vector<int>& arr, int diff) {
+        
         unordered_map<int,int> dp;
-        int ans = 0;
+        int max_length = INT32_MIN;
+        for(int i = 0; i<arr.size();i++){
 
-        for(int i=0;i<n;i++){
-            //Check if the current - difference is there in map or not
-            int temp = arr[i] - difference;
-            int tempAns = 0;
+            int val = arr[i] - diff;
+            //If the value after subtracting difference already exists, then add + 1 to the length of prev existing val
+            //to the current val
+            dp[arr[i]] = 1 + dp[val];
             
-            if(dp.count(temp))
-                tempAns = dp[temp]; //Store the temp value
-
-            dp[arr[i]] = 1 + tempAns;
-
-            ans = max(ans, dp[arr[i]]);
+            max_length = max(max_length,dp[arr[i]]);
         }
-
-        return ans;
+        return max_length;
     }
 
     int main(){
