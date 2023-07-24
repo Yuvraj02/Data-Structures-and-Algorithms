cf9d835d7138f117e7180c2b436f1320b130a398
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-07-24 23:56:11 +0530

    Catalan Number

--- Dynamic Programming/unique_binary_search_trees.cpp
+++ Dynamic Programming/unique_binary_search_trees.cpp
@@ -5,43 +5,44 @@ using namespace std;
  int solve(int n){
         if(n<=1)
             return 1;
+        
+        int ans = 0;
 
-        int ans= 0;
-        for(int i = 1; i <= n; i++){
-            ans += solve(i-1) * solve(n-i);
+        for(int i = 1; i<=n;i++){
+            ans += solve(i-1)* solve(n-i);
         }
 
         return ans;
-}
+    }
 
-int solveMem(int n,vector<int>& dp){
+    int solveMem(int n, vector<int> &dp){
 
-    if(n<=1)
-        return 1;
+        if(n<=1)
+            return 1;
 
-    if(dp[n]!=-1)
-        return dp[n];
+        if(dp[n]!=-1)
+            return dp[n];
 
-    int ans = 0;
-    for(int i = 1; i<=n; i++)
-        ans += solveMem(i-1,dp) * solveMem(n-i,dp);
+        int ans = 0;
 
-    return dp[n] = ans;
-
-}
-
- int solveTab(int n) {
-        vector<int> dp(n+1, 1);
-
-        for(int i = 2; i<=n; i++){
-            int ans = 0;
-            for(int j=1; j<=i;j++){
-                ans+=dp[j-1] * dp[i-j];
-            }
-            dp[i] = ans;
+        for(int i = 1; i<=n;i++){
+            ans += solveMem(i-1,dp) * solveMem(n-i,dp);
         }
 
-        //return solve(n,dp);
+        return dp[n] = ans;
+    }
+
+    int solveTab(int n){
+
+        vector<int> dp(n+1,1);
+
+        for(int i = 2; i<=n;i++){
+            int ans = 0;
+                for(int j = 1; j<=i; j++){
+                    ans += dp[j-1] * dp[i-j];
+                }
+            dp[i] = ans; 
+        }
         return dp[n];
     }
 
