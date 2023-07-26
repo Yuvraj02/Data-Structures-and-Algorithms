4e0b1be211780b947a506230a6c04615dae76282
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-07-27 00:00:50 +0530

    Cut into segments

--- Dynamic Programming/cut_into_segments.cpp
+++ Dynamic Programming/cut_into_segments.cpp
@@ -1,64 +1,64 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
-
-
-//Recursion + Memoization
-int cut(int n, int x, int y, int z, vector<int> &dp){
-
-    if(n==0)
-        return 0;
+int solve(int n, int x, int y, int z,vector<int> &dp){
+        
+        if(n==0)
+            return 0;
+        
+        if(n<0)
+            return INT_MIN;
+            
+        if(dp[n]!=-1)
+            return dp[n];
+            
+        int a = 1 + solve(n-x, x,y,z,dp);
+        
+        int b = 1 + solve(n-y, x,y,z,dp);
+        
+        int c = 1 + solve(n-z, x,y,z,dp);
+        
+        return dp[n] = max({a,b,c});
+        
+    }
     
-    if(n<0)
-        return INT32_MIN;
-    
-    if(dp[n]!=-1)
+    int solveTab(int n, int x, int y, int z){
+        
+        vector<int> dp(n+1, 0);
+        dp[0] = 0;
+        for(int i = 1; i<=n;i++){
+            
+            int a=INT_MIN,b=INT_MIN,c=INT_MIN;
+            
+            if(i-x>=0)
+                 a = 1 + dp[i-x];
+            if(i-y>=0)
+                 b = 1 + dp[i-y];
+            if(i-z>=0)
+                 c = 1 + dp[i-z];
+            
+            dp[i] = max({a,b,c});
+        }
         return dp[n];
-
-    int ans,maxi = INT32_MIN;
+    }
     
-    //Solve for x
-    ans = cut(n-x, x, y, z, dp);
-    if(ans!=INT32_MIN){
-        maxi = max(maxi, 1+ans);
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        //Your code here
+        //vector<int> dp(n+1,-1);
+        //int ans = solve(n,x,y,z,dp);
+        
+        int ans = solveTab(n,x,y,z);
+        return ans < 0 ? 0 : ans;
     }
-    //Solve for y
-    ans = cut(n-y, x, y, z,dp);
-    if(ans!=INT32_MIN){
-        maxi = max(maxi, 1+ans);
-    }
-    //Solve for z
-    ans = cut(n-z, x, y, z,dp);
-    if(ans!=INT32_MIN){
-        maxi = max(maxi, 1+ans);
-    }
-
-    dp[n] = maxi;
-    return dp[n];
-}
 
 int main(){
 
     int n = 7, x = 5, y=2,z=2;
-    vector<int> dp(n+1, INT32_MIN);
 
-	dp[0] = 0;
-
-	//int ans = cut(n,x,y,z,dp);
-    
-    //------Tabulation
-	for(int i=1; i <= n;i++){
-       if(i-x>=0)
-        dp[i] = max(dp[i], dp[i-x]+1);
-
-       if(i-y>=0)
-        dp[i] = max(dp[i], dp[i-y]+1);
-
-        if(i-z>=0)
-        dp[i] = max(dp[i], dp[i-z]+1);
-	}
-
-    cout<<INT32_MIN<<endl;
+    cout<<maximizeTheCuts(n,x,y,z)<<endl;
 
     return 0;
 }
