dc8ab859751c7a845cf0955586ba931a4cfc9851
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-07-28 21:54:18 +0530

    Triangle

---
+++ Patterns/triangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void triangle(int n) {
+	// Write your code here
+
+	for(int i = 1; i<=n;i++){
+		for(int j = 1; j<=i;j++){
+			cout<<i<<" ";
+		}
+	cout<<endl;
+	}
+}
