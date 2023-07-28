84a3494872d25bae7108ca62312d16cce8838f73
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2023-07-28 22:30:08 +0530

    N Forest

---
+++ Patterns/n_forst.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void nForest(int n) {
+	// Write your code here.
+	for(int i = 0; i<n;i++){
+		for(int j= 0; j<n;j++){
+			cout<<"*"<<" ";
+		}
+		cout<<endl;
+	}
+}
